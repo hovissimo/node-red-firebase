2cf796aaf8949a38bbb6e94af12c77ba5dfbbfdb
Hovis <github@bunkercode.com>
2014-07-31 22:17:49 -0700

    Merged FirebaseAdd and FirebaseSet into FirebaseModify and added Push and Remove verbs.  Updated demo_flows.json to use new FirebaseModify node.

--- firebase.js
+++ firebase.js
@@ -2,18 +2,20 @@ module.exports = function(RED) {
     "use strict";
     var Firebase = require("firebase");
 
+    function sendMessageFromSnapshot(snapshot) {
+        var msg = {};
+        msg.href = snapshot.ref().toString();
+        msg.payload = snapshot.val();
+        this.send(msg);
+    }
+
     // This Constructor is used to initialize nodes at deploy time
     function FirebaseWatch(n) {
         RED.nodes.createNode(this,n);
 
         this.firebaseurl = n.firebaseurl;
         this.firebase = new Firebase(this.firebaseurl);
-        this.onValue = (function(snapshot) {
-            var msg = {};
-            msg.href = this.firebaseurl;
-            msg.payload = snapshot.val();
-            this.send(msg);
-        }).bind(this);
+        this.onValue = sendMessageFromSnapshot.bind(this);
 
         this.firebase.on("value", this.onValue);
 
@@ -36,42 +38,44 @@ module.exports = function(RED) {
             var childpath = (this.child) ? String(msg[this.child]) : ""; // get path from msg or default to /
             childpath = (childpath.indexOf("/") == 0) ? childpath : "/" + childpath; // make sure the path starts with /
 
-            this.firebase.child(childpath).once('value', (function(snapshot) {
-                msg.href = this.firebaseurl + childpath;
-                msg.payload = snapshot.val();
-                this.send(msg);
-            }).bind(this));
+            this.firebase.child(childpath).once('value', sendMessageFromSnapshot.bind(this));
         });
     }
     RED.nodes.registerType("firebase query", FirebaseQuery);
 
-    function FirebaseSet(n) {
+    function FirebaseModify(n) {
         RED.nodes.createNode(this,n);
-
         this.firebaseurl = n.firebaseurl;
         this.child = n.child;
+        this.method = n.method;
         this.firebase = new Firebase(this.firebaseurl);
 
-        this.on('input', function(msg) {
-            var childpath = (this.child) ? msg[this.child] : ""; // get path from msg or default to /
-            childpath = (childpath.indexOf("/") == 0) ? childpath : "/" + childpath; // make sure the path starts with /
-            this.firebase.child(childpath).set(msg.payload);
-        });
+        switch (this.method) {
+            case "set":
+            case "update":
+            case "push":
+                // To prevent code repetition, call the Firebase API function based on method directly
+                this.on('input', function(msg) {
+                    // get path from msg or default to /
+                    var childpath = (this.child) ? msg[this.child] : "";
+                    // make sure the path starts with /
+                    childpath = (childpath.indexOf("/") == 0) ? childpath : "/" + childpath;
+
+                    this.firebase.child(childpath)[this.method](msg.payload);
+                });
+                break;
+            case "remove":
+                // Remove method expects first argument to be a function, so we call it differently
+                this.on('input', function(msg) {
+                    // get path from msg or default to /
+                    var childpath = (this.child) ? msg[this.child] : "";
+                    // make sure the path starts with /
+                    childpath = (childpath.indexOf("/") == 0) ? childpath : "/" + childpath;
+
+                    this.firebase.child(childpath)[this.method]();
+                });
+                break;
+        }
     }
-    RED.nodes.registerType("firebase set", FirebaseSet);
-
-    function FirebaseAdd(n) {
-        RED.nodes.createNode(this,n);
-
-        this.firebaseurl = n.firebaseurl;
-        this.child = n.child;
-        this.firebase = new Firebase(this.firebaseurl);
-
-        this.on('input', function(msg) {
-            var childpath = (this.child) ? msg[this.child] : ""; // get path from msg or default to /
-            childpath = (childpath.indexOf("/") == 0) ? childpath : "/" + childpath; // make sure the path starts with /
-            this.firebase.child(childpath).push(msg.payload);
-        });
-    }
-    RED.nodes.registerType("firebase add", FirebaseAdd);
+    RED.nodes.registerType("firebase modify", FirebaseModify);
 }
