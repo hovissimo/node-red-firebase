b3b316339e51ee3e8a299aa1020134a37911b32d
hovis <hovis@bunkercode.com>
2014-08-01 15:12:52 -0700

    Query node was resetting the msg, updated to persist existing message instead.

--- firebase_query.js
+++ firebase_query.js
@@ -2,8 +2,7 @@ module.exports = function(RED) {
     "use strict";
     var Firebase = require("firebase");
 
-    function sendMessageFromSnapshot(snapshot) {
-        var msg = {};
+    function sendMessageFromSnapshot(msg, snapshot) {
         msg.href = snapshot.ref().toString();
         msg.payload = snapshot.val();
         this.send(msg);
@@ -19,7 +18,7 @@ module.exports = function(RED) {
             var childpath = (this.child) ? String(msg[this.child]) : ""; // get path from msg or default to /
             childpath = (childpath.indexOf("/") == 0) ? childpath : "/" + childpath; // make sure the path starts with /
 
-            this.firebase.child(childpath).once('value', sendMessageFromSnapshot.bind(this));
+            this.firebase.child(childpath).once('value', sendMessageFromSnapshot.bind(this, msg));
         });
     }
     RED.nodes.registerType("firebase query", FirebaseQuery);
